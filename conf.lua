53e17fd28a5eb6e9d33a9354382a97e8a540f0f8
Masayuki Sugahara <brindflontia@gmail.com>
2017-03-24 19:56:49 +0900

    added icon

--- conf.lua
+++ conf.lua
@@ -2,6 +2,7 @@ function love.conf(c)
   c.title = "JUMPER"
   c.identity = "jumper"
   c.version = "0.10.2"
+  c.window.icon = "img/jumper.png"
   c.modules.joystick = false
   c.modules.physics = false
   c.externalstorage = false
